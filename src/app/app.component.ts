01485cd1172974c00cdde2cc7bd6306951f53b0b
Abhishek Mohan <abhishekmohan@Abhisheks-MacBook-Pro.local>
2022-06-21 15:23:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'input-form';
+  textCount = "";
+
+  onUpdate(event: Event) {
+    //console.log(event);
+	this.textCount = (<HTMLInputElement>event.target).value;
+  }
+
+ 
 }
